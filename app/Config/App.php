eb7ac0d7e24b117059c6cd6c3a3666781d6fed3f
Jhonatan <jhonatann36@gmail.com>
2021-08-02 00:04:16 -0300

    traduz mensagens padrões pt-br

--- app/Config/App.php
+++ app/Config/App.php
@@ -69,7 +69,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $defaultLocale = 'en';
+	public $defaultLocale = 'pt-br';
 
 	/**
 	 * --------------------------------------------------------------------------
@@ -96,7 +96,7 @@ class App extends BaseConfig
 	 *
 	 * @var string[]
 	 */
-	public $supportedLocales = ['en'];
+	public $supportedLocales = ['en', 'pt-br'];
 
 	/**
 	 * --------------------------------------------------------------------------
@@ -108,7 +108,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $appTimezone = 'America/Chicago';
+	public $appTimezone = 'America/Sao_Paulo';
 
 	/**
 	 * --------------------------------------------------------------------------
